1b4cffdf273ecd866e94f4e7d230541370ea952b
Rameshraju7 <mallelaramesh5@gmail.com>
2016-07-27 14:51:03 +0530

    SumOfTwoNumbers

---
+++ SumOfTwoNumb.java
@@ -0,0 +1,14 @@
+
+public class SumOfTwoNumb {
+	public static void main(String[] args){
+		int a=5;
+		float b=19.5f,c=a+b;
+		System.out.println("a value is :"+a);
+		System.out.println("b value is:"+b);
+		
+		System.out.print("SumOfTwoNum:"+c);
+		
+			
+	}
+
+}
